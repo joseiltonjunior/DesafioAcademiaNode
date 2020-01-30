3b8c44a4ee54b5d84998beaace66681342023c55
joseiltonjunior <joseiltonjuniortech@gmail.com>
2020-01-30 14:17:25 -0300

    Complementando o curriculo

--- src/controllers/CurriculoController.js
+++ src/controllers/CurriculoController.js
@@ -6,26 +6,48 @@ module.exports = {
   async show(req, res) {
 
     try {
-      const apiFace = await axios.get(`https://graph.facebook.com/v5.0/me?fields=name%2Cbirthday%2Cgender%2Cemail%2Clocation%2Cphotos.limit(1)%7Bpicture%7D&access_token=${process.env.FACE_TOKEN}`);
+      const apiFace = await axios.get(`https://graph.facebook.com/v5.0/me?fields=name%2Cbirthday%2Cgender%2Cemail%2Clocation%2Cphotos.limit(1)%2Caddress&access_token=${process.env.FACE_TOKEN}`);
       const apiGit = await axios.get('https://api.github.com/users/joseiltonjunior');
 
-      const { name = login, html_url, bio, company, repos_url, avatar_url } = apiGit.data;
+      const { name = login, html_url, bio, repos_url, avatar_url } = apiGit.data;
       const { birthday, gender, email, location } = apiFace.data;
 
       const profile = {
-        "facebook_profile": {
-          "Foto": avatar_url,
-          "Nome: ": name,
-          "Endereço: ": location.name,
-          "Genero: ": gender,
-          "Aniversário": birthday,
-          "E-mail: ": email,
-        },
-        "github_profile": {
-          "Nome: ": name,
+
+        "Nome: ": name,
+        "Data de nascimento": birthday,
+        "Endereço: ": location.name,
+        "E-mail: ": email,
+        "Genero: ": gender,
+        "Bio: ": bio,
+        "Foto": avatar_url,
+
+        "Formação": [{
+          "Instituição: ": "Faculdade Imaculada Conceição do Recife",
+          "Curso: ": "ADS",
+          "Inicio: ": "2019.1",
+          "Termino: ": "2021.2",
+        }],
+
+        "Experiência Profissional": [
+          {
+            "Empresa": "Exército Brasileiro",
+            "Função": "Suporte Técnico",
+            "Atividade": "Realizar manutenção em computadores, impressoras e rede",
+            "Inicio": "12/01/2019",
+            "Termino": "atual"
+          },
+          {
+            "Empresa": "Exército Brasileiro",
+            "Função": "Auxiliar Admnistrativo",
+            "Atividade": "Elaboração de documentos e planilhas administrativas, protocolo e arquivamento de documentos.",
+            "Inicio": "03/03/2015",
+            "Termino": "atual"
+          },          
+        ],
+
+        "GitHub": {
           "Github: ": html_url,
-          "Bio: ": bio,
-          "Local de Trabalho: ": company,
           "Repositório: ": repos_url
         }
       };
